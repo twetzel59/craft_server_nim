a2485cb113693248fe21e65ffb85b653930709f9
twetzel59 <twetzel59@gmail.com>
2018-05-18 17:19:43 -0400

    Basic client/server architecture

--- src/craft_server_nimpkg/server.nim
+++ src/craft_server_nimpkg/server.nim
@@ -1,5 +1,31 @@
-import settings
+import std / net
+import client, settings
+from utils as ut import nil
+
+type
+  ServerSocket = Socket not nil
+
+  Server = object
+    clients: ut.Seq[Client]
+    socket: ServerSocket
+
+proc initServer(): Server =
+  Server(clients: @[], socket: newSocket())
+
+proc bindListen(s: Server; settings: Settings) =
+  s.socket.bindAddr(settings.port)
+  s.socket.listen()
 
 proc serverBegin*(settings: Settings) =
   echo "Using world file: ", settings.worldFile
   echo "Using port: ", $int16(settings.port)
+
+  var s = initServer()
+  s.bindListen(settings)
+
+  while true:
+    var clientSocket = new Socket
+    var address = ""
+    s.socket.acceptAddr(clientSocket, address)
+
+    echo address, " is connecting"
--- src/craft_server_nimpkg/utils.nim
+++ src/craft_server_nimpkg/utils.nim
@@ -1,3 +1,6 @@
+type
+  Seq*[T] = seq[T] not nil
+
 proc notNilOrDie*(arg: string): string not nil =
   if arg.isNil:
     raise newException(AssertionError, "notNilOrDie: arg was nil")
