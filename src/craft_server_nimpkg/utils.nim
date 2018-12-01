5dafef54b2fd4914cb4bc4df8bce44b9728f1724
twetzel59 <twetzel59@gmail.com>
2018-12-01 12:37:19 -0500

    Start over for the new design

--- src/craft_server_nimpkg/utils.nim
+++
@@ -1,5 +0,0 @@
-proc notNilOrDie*(arg: string): string not nil =
-  if arg.isNil:
-    raise newException(AssertionError, "notNilOrDie: arg was nil")
-  else:
-    return arg
--- src/craft_server_nimpkg/vec.nim
+++
@@ -1,9 +0,0 @@
-type
-  Vec2*[T] = tuple[x, y: T]
-  Vec2f* = Vec2[float]
-
-  Vec3*[T] = tuple[x, y, z: T]
-  Vec3f* = Vec3[float]
-
-  Pos3Rot2*[T] = tuple[pos: Vec3[T], rot: Vec2[T]]
-  Pos3Rot2f* = Pos3Rot2[float]
