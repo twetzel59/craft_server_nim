9bb04a32d0d20d9da8f8f509c2418d0184e6d59e
twetzel59 <twetzel59@gmail.com>
2018-12-01 17:32:36 -0500

    Add basic async TCP server

--- src/craft_server_nim.nim
+++ src/craft_server_nim.nim
@@ -1,5 +1,55 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import asyncdispatch, asyncnet
+
+const
+  craftPort = Port(4080)
+
+type
+  Client = ref object
+    ipStr: string
+    socket: AsyncSocket
+    id: int
+    connected: bool
+
+  Server = ref object
+    socket: AsyncSocket
+    clients: seq[Client]
+
+func newClient(accepted: tuple[address: string, client: AsyncSocket]; id: int): Client =
+  Client(
+    ipStr: accepted[0],
+    socket: accepted[1],
+    id: id,
+    connected: true
+  )
+
+func newServer(servSocket: AsyncSocket): Server =
+  Server(socket: servSocket)
+
+proc clientLoop(client: Client) {.async.} =
+  while true:
+    let line = await recvLine client.socket
+
+    if len(line) == 0:
+      # Client disconnected.
+      echo "Disconnecting: ", client.ipStr
+      return
+
+proc servLoop(serv: Server) {.async.} =
+  bindAddr serv.socket, craftPort
+  listen serv.socket
+
+  while true:
+    let accepted = await acceptAddr serv.socket
+    echo "Connecting: ", accepted[0]
+
+    let client = newClient(accepted, len serv.clients)
+    #add serv.clients, client
+
+    asyncCheck clientLoop(client)
+
+proc main() =
+  let serv = newServer newAsyncSocket()
+  waitFor servLoop serv
 
 when isMainModule:
-  echo("Hello, World!")
+  main()
